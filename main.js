d76694ba03d87ab75fee885c6a5036042339ba73
Matt Kiefer <mattkiefer@gmail.com>
2020-06-25 11:20:34 -0400

    Putting initial pieces in place; wrestling wtih calling a function from a variable name

---
+++ main.js
@@ -0,0 +1,20 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+function operate(operation, n1, n2) {
+    let op = operation;
+    return window[op](n1, n2);
+}
