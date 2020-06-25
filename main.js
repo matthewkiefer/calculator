74922adda2161912759eb1e47ebd3085e04190dc
Matt Kiefer <mattkiefer@gmail.com>
2020-06-25 12:48:03 -0400

    solved calling operations with an operations object

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
+const operations = {
 
-function subtract(n1, n2) {
-    return n1 - n2;
-}
+    add: function(n1, n2) {
+        return n1 + n2;
+    },
 
-function multiply(n1, n2) {
-    return n1 * n2;
-}
+    subtract: function(n1, n2) {
+        return n1 - n2;
+    },
 
-function divide(n1, n2) {
-    return n1 / n2;
+    multiply: function(n1, n2) {
+        return n1 * n2;
+    },
+
+    divide: function(n1, n2) {
+        return n1 / n2;
+    },
+
 }
 
 function operate(operation, n1, n2) {
-    let op = operation;
-    return window[op](n1, n2);
+    return operations[operation](n1, n2);
 }
