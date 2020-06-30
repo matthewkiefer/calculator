f338115059aff3e94e83aeb08bcc45d847825abd
Matt Kiefer <mattkiefer@gmail.com>
2020-06-29 21:17:48 -0400

    working and styled but with bugs; going to try alternating array slots for numbers and operands

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+//what to do after equation: if more numbers, clear. (set a toggle after compute?)
+//if operands, continue.
+// -currently trying with "hasLooped"
+//-grey out . if not available?
+
 const operations = {
     add: function(n1, n2) {
         return n1 + n2;
@@ -13,9 +18,10 @@ const operations = {
     },
 }
 
-function operate(operation, n1, n2) {
-    return operations[operation](n1, n2);
-}
+// **called for in TOP instructions but didn't end up using
+// function operate(operation, n1, n2) {
+//     return operations[operation](n1, n2);
+// }
 
 function compute() { //Loop through the equation array, once for */ and once for +-
     //loop for * and /
@@ -50,40 +56,74 @@ function clear() {
     digitsString = "";
     equation.splice(0, equation.length);
     display.textContent = "";
+    hasLooped = false;
+}
+
+function turnOffEnterGlow() {
+    let enter = document.getElementById("enter");
+    enter.classList.remove("glowing");
+}
+
+function turnOnEnterGlow() {
+    let enter = document.getElementById("enter");
+    enter.classList.add("glowing");
 }
 
 const equation = [];
 let digitsString = "";
 let display = document.getElementById("display-numbers")
 let buttons = document.querySelector("#main-grid");
+let hasLooped = false;
 
 function handleClicks(e) {
-    let selection = "";
+    let selection = ""; //the selected number or operand, added to the display at end of function
+
+  console.log(digitsString);
+    if (e.target.id == "dot" && digitsString.indexOf(".") !== -1) {
+       return;
+    }
+
     if (e.target.className == "numbers buttons" || 
             e.target.className == "operators buttons") {
-        selection = e.target.id;
+        selection = e.target.textContent;
     }
+
     if (e.target.className == "numbers buttons") { 
-          digitsString += e.target.id.toString();
+          digitsString += e.target.textContent.toString();
     }
+
     if (e.target.className == "operators buttons") {
-        equation.push(+digitsString);
-        equation.push(e.target.id);
+        if (digitsString !== "") { 
+            equation.push(+digitsString); 
+        }
+        equation.push(e.target.textContent);
         digitsString = "";
     }
     if (e.target.id == "enter") {
         equation.push(+digitsString);
         compute();
+        turnOffEnterGlow();
         display.textContent = equation[0]; //update display to final answer
         selection = "";
+        digitsString = "";
+        hasLooped = true;
     }
     if (e.target.id == "clear") {
         clear();
         selection = "";
     }
+
+    //glowing enter button if ready; length means at least a digit and an operand
+    //and the next digit is waiting in digitsString 
+    if (equation.length > 1 && digitsString) {
+        turnOnEnterGlow();
+    }
+    
     display.textContent += selection;
+    
 }
 
 clear();
 buttons.addEventListener("click", handleClicks);
+
 
